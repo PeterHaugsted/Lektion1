c25c17812f193645ff8d1591a56faa7d8a6da7b0
SourSolution <peterhaugsted@hotmail.com>
2022-01-31 16:43:38 +0100

    Lovely message that explains things properly

--- src/Lektion1/Opgave1.java
+++ src/Lektion1/Opgave1.java
@@ -4,16 +4,6 @@ public class Opgave1 {
 
 
     public static void main(String[] args) {
-
-        int test = 10;
-        test += 1509000;
-        test += returnTen();
         System.out.println("Hello DTU!");
     }
-
-    public static int returnTen() {
-        return 10;
-    }
-
-
 }
